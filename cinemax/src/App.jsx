43e95f25915d137429c04dcc0a6d9c2dbba3a159
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2023-03-19 16:53:13 -0300

    feat:  implemented routes and outlet

--- cinemax/src/App.jsx
+++ cinemax/src/App.jsx
@@ -1,11 +1,18 @@
+import { Link, Outlet } from 'react-router-dom';
 import './App.css'
+// import {Link, Outlet} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <h2>OlaMundo</h2>
+    <div>
+      <nav id="id">
+        <h2><Link to="/">Home</Link></h2>
+        <h2><Link to="movie/1">Movie</Link></h2>
+        <h2><Link to="search">Search</Link></h2>
+      </nav>
+      <Outlet />
     </div>
-  )
+  );
 }
 
 export default App
