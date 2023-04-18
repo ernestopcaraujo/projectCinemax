fce9b70a410b63d6623f0fb1dc9ad040a23bba02
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2023-04-17 22:51:20 -0300

    feat: added styles to html tags

--- cinemax/src/pages/Home.jsx
+++ cinemax/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import MovieCard from "../components/MovieCard";
+import './MoviesGrid.css'
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -34,8 +35,8 @@ const Home = () => {
 
         return(
         <div className="container">
-            <h2 className="title">Melhores Filmes</h2>
-            <div>
+            <h2 className="title">Nossos Top 20 Filmes</h2>
+            <div className="movies-container">
                 {topMovies.length === 0 && <p>Carregando...</p>}
                 {topMovies.length > 0  && topMovies.map((movie)=><MovieCard key={movie.id} movie={movie}/>)}  
             </div>
