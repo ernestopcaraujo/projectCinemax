9e0b0214d5812ac9b94b2c998427a502919c6913
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2023-04-17 22:51:55 -0300

    feat: changed description texts

--- cinemax/src/components/Navbar.jsx
+++ cinemax/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom"
 import { BiCameraMovie, BiSearchAlt2} from "react-icons/bi"
+import './Navbar.css'
 
 const Navbar = () => {
     return(
-        <nav id="id">
+        <nav id="navbar">
             <h2>
                 <Link to="/">
-                    <BiCameraMovie />HOME
+                    <BiCameraMovie />CineDEV
                 </Link></h2>
             <form>
                 <input type="text" placeholder="Buscar filme" />
