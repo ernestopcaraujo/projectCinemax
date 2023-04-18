1071793732d96d180474dc2001a82966b1b77523
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2023-04-17 22:52:45 -0300

    feat: changed code layout

--- cinemax/src/components/MovieCard.jsx
+++ cinemax/src/components/MovieCard.jsx
@@ -5,19 +5,14 @@ const imageURL=import.meta.env.VITE_IMG;
 
 const MovieCard = ({movie, showLink=true}) => {
 
-
-
-
-
-
-
+    
     return (
-    <div className='movie-card'>
-        <img src={`${imageURL}${movie.poster_path}`} alt={movie.title} />
-        <h2>{movie.title}</h2>
-        <p><FaStar />{movie.vote_average}</p>
-        {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
-    </div>
+        <div className='movie-card'>
+            <img src={`${imageURL}${movie.poster_path}`} alt={movie.title} />
+            <h2>{movie.title}</h2>
+            <p><FaStar />{movie.vote_average}</p>
+            {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
+        </div>
     )
 }
 
