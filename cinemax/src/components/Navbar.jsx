6c3f625f735af1387a21cb6ef5dac4f6069f6213
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2023-04-20 07:11:59 -0300

    feat: added code to handle the movie search

--- cinemax/src/components/Navbar.jsx
+++ cinemax/src/components/Navbar.jsx
@@ -1,17 +1,31 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 import { BiCameraMovie, BiSearchAlt2} from "react-icons/bi"
 import './Navbar.css'
 
 const Navbar = () => {
+    const [search, setSearch] = useState("");
+    const navigate = useNavigate();
+    const handleSubmit  = (e)=>{
+        e.preventDefault();//para não submeter o form como uma requisicao http
+        
+        if (!search) return; //verifica se está vazio a variável search
+        navigate(`/search?q=${search}`);
+        setSearch('');//limpar o search
+    }
     return(
         <nav id="navbar">
             <h2>
                 <Link to="/">
                     <BiCameraMovie />CineDEV
                 </Link></h2>
-            <form>
-                <input type="text" placeholder="Buscar filme" />
-                <button type="submit"><BiSearchAlt2></BiSearchAlt2></button>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Buscar filme"
+                    onChange={(e)=>setSearch(e.target.value)}
+                    value={search} //aqui é para limpar o campo depois da digitação
+                 
+                 />
+                <button type="submit"><BiSearchAlt2 /></button>
             </form>
         </nav>
     )
